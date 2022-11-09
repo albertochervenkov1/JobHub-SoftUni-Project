b28cd405b7910eb74a0986fc70ff0ddb7da5aa3b
Alberto Chervenkov <64748315+albertotch@users.noreply.github.com>
2022-11-09 23:30:35 +0200

    Identity

---
+++ JobHub/JobHub/Controllers/UserController.cs
@@ -0,0 +1,114 @@
+﻿using JobHub.Infrastructure.Data.Models;
+using JobHub.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobHub.Controllers
+{
+    public class UserController : BaseController
+    {
+        private readonly UserManager<User> userManager;
+        private readonly SignInManager<User> signInManager;
+
+        public UserController(UserManager<User> _userManager,
+            SignInManager<User> _signInManager)
+        {
+            userManager = _userManager;
+            signInManager = _signInManager;
+        }
+
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            var model = new RegisterViewModel();
+
+            return View(model);
+        }
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = new User()
+            {
+                Email = model.Email,
+                FirstName = model.FirstName,
+                EmailConfirmed = true,
+                LastName = model.LastName,
+                UserName = model.Email
+            };
+
+            var result = await userManager.CreateAsync(user, model.Password);
+           
+
+            if (result.Succeeded)
+            {
+                await signInManager.SignInAsync(user, isPersistent: false);
+
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var item in result.Errors)
+            {
+                ModelState.AddModelError("", item.Description);
+            }
+
+
+            return View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var model = new LoginViewModel();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            var user = await userManager.FindByNameAsync(model.UserName);
+
+            if (user != null)
+            {
+                var result = await signInManager.PasswordSignInAsync(user, model.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            ModelState.AddModelError("", "Invalid login");
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ JobHub/JobHub/Models/RegisterViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using JobHub.Infrastructure.Common;
+
+namespace JobHub.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [Compare(nameof(PasswordRepeat))]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string PasswordRepeat { get; set; } = null!;
+
+        [Required]
+        [StringLength(UserConstraints.FIRST_NAME_MAX_LENGTH, MinimumLength = UserConstraints.FIRST_NAME_MIN_LENGTH)]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [StringLength(UserConstraints.LAST_NAME_MAX_LENGTH, MinimumLength = UserConstraints.LAST_NAME_MIN_LENGTH)]
+        public string LastName { get; set; } = null!;
+    }
+}
